ff86cc4ae3f5dc2f0d17378ab3891718cd8e0ad3
LouisLapointe01 <114722726+LouisLapointe01@users.noreply.github.com>
2022-11-25 09:17:12 +0100

    TP 63Prct
    TP 63Prct

---
+++ ilu1-tp5.ml
@@ -0,0 +1,25 @@
+let rec insertObj o liste =
+  match liste with 
+  |[] -> (1,o):: liste |(x,y):: list-> if y=o then ((x+1),y)::list else(x,y):: insertObj o list;;
+
+let rec insert p o liste = 
+  match liste with 
+  | [] ->(p,((1,o)::[]))::liste
+  |(x,y)::list->
+      if x = p then (x,(insertObj o y)):: list
+      else (x,y) :: insert p o list;;
+
+let rec decompte liste = 
+  match liste with
+  |[] -> []
+  |(x,y):: list -> insert x y (decompte list);;
+
+let rec longueur liste = match liste with 
+  |[] -> 0
+  |(x,y):: list -> x + longueur list;;
+
+let rec aumoins_indexed n liste = 
+  match liste with
+  |[]-> []
+  |(x,y):: list -> if (longueur y) >= n then x :: aumoins_indexed n liste
+      else aumoins_indexed n list;;
