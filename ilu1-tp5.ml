8f5b606b211a2554d8f218361d0074b5d5d9e09c
Louis Lapointe <114722726+LouisLapointe01@users.noreply.github.com>
2022-11-25 16:54:16 +0100

    Add files via upload

--- ilu1-tp5.ml
+++ ilu1-tp5.ml
@@ -1,6 +1,7 @@
 let rec insertObj o liste =
   match liste with 
-  |[] -> (1,o):: liste |(x,y):: list-> if y=o then ((x+1),y)::list else(x,y):: insertObj o list;;
+  |[] -> (1,o):: liste |(x,y):: list-> if y=o 
+      then ((x+1),y)::list else(x,y):: insertObj o list;;
 
 let rec insert p o liste = 
   match liste with 
@@ -18,8 +19,10 @@ let rec longueur liste = match liste with
   |[] -> 0
   |(x,y):: list -> x + longueur list;;
 
-let rec aumoins_indexed n liste = 
-  match liste with
+let rec aumoins_indexed n l = 
+  match l with
   |[]-> []
-  |(x,y):: list -> if (longueur y) >= n then x :: aumoins_indexed n liste
-      else aumoins_indexed n list;;
+  |(x,y)::l' -> if (longueur y) >= n then x :: (aumoins_indexed n l')
+      else (aumoins_indexed n l');;
+
+let aumoins n l = aumoins_indexed n (decompte l)
